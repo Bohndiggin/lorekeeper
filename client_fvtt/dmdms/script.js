51ad656ec2e184284891758742a92967e2e1c887
bohndiggin <bohndiggin@gmail.com>
2023-12-25 18:05:02 -0700

    module work

--- client_fvtt/dmdms/script.js
+++ client_fvtt/dmdms/script.js
@@ -3,6 +3,49 @@ const connection = "http://127.0.0.1:8000";
 let displayArea = document.getElementById('object-display')
 let singleDisplay = document.getElementById('single-display')
 
+Hooks.on("renderSidebarTab", async (app, html) => {
+    if (app instanceof JournalDirectory) {
+      let button = $("<button class='dmdms'>DMDMS</button>")
+  
+      button.click(function () {
+        new DMDMS().render(true);
+      });
+  
+      html.find(".directory-footer").append(button);
+    }
+  })
+  
+  Hooks.on("init", () => {
+    game.settings.register("dmdms", "settings", {
+      name: "Server Location",
+      scope: "world",
+      config: false,
+      default: {
+        server: 'http://127.0.0.1:8000'
+      }
+    })
+  })
+  
+  
+  
+  
+  class DMDMS extends FormApplication {
+  
+  
+    static get defaultOptions() {
+      const options = super.defaultOptions;
+      options.id = "dmdms";
+      options.template = "modules/dmdms/index.html"
+      options.classes.push("dmdms");
+      options.resizable = false;
+      options.height = "auto";
+      options.width = 400;
+      options.minimizable = true;
+      options.title = "Dungeon Master Data Managment System"
+      return options;
+    }
+  }
+  
 
 
 function writeTable(response, endpoint){
