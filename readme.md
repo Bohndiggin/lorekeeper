4c0b19f9b77aef8b263a7a33969630c7f9463344
Bohndiggin <bohndiggin@gmail.com>
2024-02-29 09:15:36 -0700

    adding docs

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # LOREKEEPER #
 
-## Description
+## Overview
 
 Lorekeeper is the answer to a problem I've personally had with managing dungeons and dragons campaigns. As such it has many roles to fill of its own.
 
@@ -11,3 +11,117 @@ Then it hit me. SQL. PostgreSQL is a near infinite tool to make and link data. N
 Lorekeeper is in its infancy, but as they say "it works on my machine"
 
 Humor aside, more details are to follow...
+
+# Back End
+
+## Actors
+
+Actors are any character, from player, to POV to NPC.
+
+
+Here's the scema for Actors:
+
+```pgsql
+id SERIAL PRIMARY KEY,
+first_name TEXT,
+middle_name TEXT,
+last_name TEXT,
+title TEXT,
+actor_age INT,
+class_id INT,
+actor_level INT,
+background_id INT,
+job TEXT,
+actor_role TEXT,
+race_id INT,
+sub_race_id INT,
+alignment VARCHAR(2),
+strength INT,
+dexterity INT,
+constitution INT,
+intelligence INT,
+wisdom INT,
+charisma INT,
+ideal TEXT,
+bond TEXT,
+flaw TEXT,
+appearance TEXT,
+strengths TEXT,
+weaknesses TEXT,
+notes TEXT,
+
+FOREIGN KEY (class_id) REFERENCES classes(id),
+FOREIGN KEY (background_id) REFERENCES background(id),
+FOREIGN KEY (race_id) REFERENCES race(id),
+FOREIGN KEY (sub_race_id) REFERENCES sub_race(id)
+```
+
+
+**_Let's break it down one element at a time:_**
+
+### id
+As any ID in PostgreSQL, this one is auto generated.
+### first_name
+First Name of the Actor
+#### Note: first_name will be used for Proper Name (shortened version)
+### middle_name
+Middle Name of the Actor (can be blank)
+### last_name
+Last Name of the Actor
+### title
+Things like Dr., Arch Mage, that kind of thing.
+### actor_age
+How old the Actor is.
+### class_id
+ID (as int) of class found in the class list.
+### actor_level
+Int of the character's level overall.
+### background_id
+ID (as int) of Background found in the Backgrounds table
+### job
+What job does this Actor do? Are they homeless? A Blacksmith?
+### actor_role
+Why is this character in your story? Are they a villian? Sidekick?
+### race_id
+ID (as int) of the characters Race found in race table
+### sub_race_id
+ID (as int) of a sub race (if any)
+### alignment
+2 letter code to represent alignment and disposition.
+
+CG NG LG
+
+CN NN LN
+
+CE NE LE
+
+### strength
+Int (typically out of 20) to represent Actor's Strength. (10=Default)
+### dexterity
+Int (typically out of 20) to represent Actor's Dexterity (10=Default)
+### constitution
+Int (typically out of 20) to represent Actor's Constitution (10=Default)
+### intelligence
+Int (typically out of 20) to represent Actor's Intelligence (10=Default)
+### wisdom
+Int (typically out of 20) to represent Actor's Wisdom (10=Default)
+### charisma
+Int (typically out of 20) to represent Actor's Charisma (10=Default)
+### ideal
+A text description of what ideals the Actor strives for. Kindness, Equality, Power, that sort of thing.
+### bond
+A text description of something that the Actor cares a great deal for. Person, place, Thing, w/e.
+### flaw
+A text description of a specific flaw the Actor has. Something that usually can be exploited.
+### appearance
+A text description of what the Actor looks like. As long as needed.
+### strengths
+A text description of what the Actor is good at. As long as needed.
+### weaknesses
+A text description of what the Actor struggles with. What they are bad at. As long as needed.
+### notes
+A place for any additional notes not covered previously.
+
+
+Lorekeeper is primarally a DND tool at the moment so some of the traits tracked are specific to DND.
+
--- server/server.py
+++ server/server.py
@@ -10,6 +10,64 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
+description = """
+Lorekeeper is a new app to manage the lore of your stories.
+
+
+## History
+Initially designed as a tool for a Dungeon Master, Lorekeeper aims to be the be all end all of lore managment.
+
+## Components
+Lorekeeper consists of a few elements.
+
+- A PostreSQL sever to store the lore.
+- A server (this file) to handle reqests to and from.
+- A few seperate front ends
+    1. a Foundry VTT module so users can access the lore from remotely
+    2. a locally hosted webpage for exploring lore
+    3. TODO: a desktop app for quickly adding lore to the database.
+
+## Usage
+
+The storyteller/DM can explore lore and add new entries to the database.
+
+TODO: Entries can be linked in logical ways.
+
+Operation should be as simple as Salesforce.
+
+## Installation
+
+**COMING SOON**
+"""
+
+
+tags_metadata = [
+    {
+        'name': 'Actors',
+        'description': 'Actors are players/NPCs both are represented here.'
+    },
+    {
+        'name': 'Faction',
+        'description': 'Factions are the groups that vie for power.'
+    },
+    {
+        'name': 'Location',
+        'description': 'Locations include cities, dungeons, any place really.'
+    },
+    {
+        'name': 'Historical Fragments',
+        'description': 'Historical Fragments are events that happened. They are split into single events for simplicity.'
+    },
+    {
+        'name': 'Objects',
+        'description': 'Objects are well, things.'
+    },
+    {
+        'name': 'World Data',
+        'description': 'World Data is lore that is constant eg: magic exists.'
+    }
+]
+
 load_dotenv()
 
 def unpack_answer(answer):
@@ -27,7 +85,15 @@ async def lifespan(app: FastAPI):
 
     print("Server Shutdown Complete")
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    title="LoreKeeper",
+    description=description,
+    summary="The Keeper or Lore",
+    version='0.1.2',
+    openapi_tags=tags_metadata
+)
+
 app.mount('/main', StaticFiles(directory='../client_browser', html=True), name='dmdms')
 
 origins = [
@@ -51,90 +117,90 @@ app.add_middleware(
 async def homepage():
     pass
 
-@app.get("/actor")
+@app.get("/actor", tags=['Actors'])
 async def actor_get():
     return utils.actor_table.query_get_10()
 
-@app.post('/actor')
+@app.post('/actor', tags=['Actors'])
 async def actor_post(body:RequestBodyList):
     return utils.actor_table.post_data(body.items_list)
 
-@app.get('/actor-names')
+@app.get('/actor-names', tags=['Actors'])
 async def actor_get_names():
     return utils.actor_table.get_all_named()
 
-@app.get('/actor/')
+@app.get('/actor/', tags=['Actors'])
 async def actor_get_one(id:int = 0):
     return utils.actor_table.query_one_by_id(id)
 
-@app.delete('/actor/')
+@app.delete('/actor/', tags=['Actors'])
 async def actor_delete_one(id:int):
     return utils.actor_table.delete_row_w_dependancies(id)
 
-@app.get('/faction')
+@app.get('/faction', tags=['Faction'])
 async def faction_get():
     return utils.faction_table.query_get_10()
 
-@app.post('/faction')
+@app.post('/faction', tags=['Faction'])
 async def faction_post(body:RequestBodyList):
     return utils.actor_table.post_data(body.items_list)
 
-@app.get('/faction/')
+@app.get('/faction/', tags=['Faction'])
 async def faction_get_one(id:int = 0):
     return utils.faction_table.query_one_by_id(id)
 
-@app.delete('/faction/')
+@app.delete('/faction/', tags=['Faction'])
 async def faction_delete_one(id:int):
     return utils.faction_table.delete_row_w_dependancies(id)
 
-@app.get('/location')
+@app.get('/location', tags=['Location'])
 async def location_get():
     return utils.location_table.query_get_10()
 
-@app.post('/location')
+@app.post('/location', tags=['Location'])
 async def location_post(body:RequestBodyList):
     return utils.location_table.post_data(body.items_list)
 
-@app.get('/location/')
+@app.get('/location/', tags=['Location'])
 async def location_get_one(id:int = 0):
     return utils.location_table.query_one_by_id(id)
 
-@app.delete('/location/')
+@app.delete('/location/', tags=['Location'])
 async def loctaion_delete_one(id:int):
     return utils.location_table.delete_row_w_dependancies(id)
 
-@app.get('/historical-fragments')
+@app.get('/historical-fragments', tags=['Historical Fragments'])
 async def historical_fragments_get():
     return utils.historical_fragments_table.query_get_10()
 
-@app.post('/historical-fragments')
+@app.post('/historical-fragments', tags=['Historical Fragments'])
 async def historical_fregments_post(body:RequestBodyList):
     return utils.historical_fragments_table.post_data(body.items_list)
 
-@app.get('/historical-fragments/')
+@app.get('/historical-fragments/', tags=['Historical Fragments'])
 async def historical_fragments_get_one(id:int = 0):
     return utils.historical_fragments_table.query_one_by_id(id)
 
-@app.get('/object')
+@app.get('/object', tags=['Objects'])
 async def object_get():
     return utils.object_table.query_get_10()
 
-@app.post('/object')
+@app.post('/object', tags=['Objects'])
 async def object_post(body:RequestBodyList):
     return utils.object_table.post_data(body.items_list)
 
-@app.get('/object/')
+@app.get('/object/', tags=['Objects'])
 async def object_get_one(id:int = 0):
     return utils.object_table.query_one_by_id(id)
 
-@app.delete('/object/')
+@app.delete('/object/', tags=['Objects'])
 async def object_delete_by_id(id:int):
     return utils.object_table.delete_row(id)
 
-@app.get('/world-data')
+@app.get('/world-data', tags=['World Data'])
 async def world_data_get():
     return utils.world_data_table.query_get_10()
 
-@app.get('/world-data/')
+@app.get('/world-data/', tags=['World Data'])
 async def world_data_get_one(id:int = 0):
     return utils.world_data_table.query_one_by_id(id)
