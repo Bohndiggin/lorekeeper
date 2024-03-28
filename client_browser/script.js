9cc068c7f8c1663c120f5deb358b39ba6e60b7c5
Bohndiggin <bohndiggin@gmail.com>
2024-03-28 07:13:17 -0600

    edit button now visible.

--- client_browser/script.js
+++ client_browser/script.js
@@ -140,7 +140,7 @@ function writeOne(data, idNum) {
     for (const property in overview) {
         singleDisplay.innerHTML += `<li><b>${formatNormal(property)}:</b> ${overview[property]}</li>`
     }
-    singleDisplay.innerHTML += `</ul><div id='edit-${idNum}' class='add-btn-centered'></div>`
+    singleDisplay.innerHTML += `</ul><div id='edit-${idNum}' class='edit-btn-centered'><svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px" viewBox="0 0 494.936 494.936" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M389.844,182.85c-6.743,0-12.21,5.467-12.21,12.21v222.968c0,23.562-19.174,42.735-42.736,42.735H67.157 c-23.562,0-42.736-19.174-42.736-42.735V150.285c0-23.562,19.174-42.735,42.736-42.735h267.741c6.743,0,12.21-5.467,12.21-12.21 s-5.467-12.21-12.21-12.21H67.157C30.126,83.13,0,113.255,0,150.285v267.743c0,37.029,30.126,67.155,67.157,67.155h267.741 c37.03,0,67.156-30.126,67.156-67.155V195.061C402.054,188.318,396.587,182.85,389.844,182.85z"></path> <path d="M483.876,20.791c-14.72-14.72-38.669-14.714-53.377,0L221.352,229.944c-0.28,0.28-3.434,3.559-4.251,5.396l-28.963,65.069 c-2.057,4.619-1.056,10.027,2.521,13.6c2.337,2.336,5.461,3.576,8.639,3.576c1.675,0,3.362-0.346,4.96-1.057l65.07-28.963 c1.83-0.815,5.114-3.97,5.396-4.25L483.876,74.169c7.131-7.131,11.06-16.61,11.06-26.692 C494.936,37.396,491.007,27.915,483.876,20.791z M466.61,56.897L257.457,266.05c-0.035,0.036-0.055,0.078-0.089,0.107 l-33.989,15.131L238.51,247.3c0.03-0.036,0.071-0.055,0.107-0.09L447.765,38.058c5.038-5.039,13.819-5.033,18.846,0.005 c2.518,2.51,3.905,5.855,3.905,9.414C470.516,51.036,469.127,54.38,466.61,56.897z"></path> </g> </g> </g></svg></div>`
     singleDisplay.innerHTML += '<h2>Traits</h2>'
     for (const property in traits) {
         singleDisplay.innerHTML += `<h3>${formatNormal(property)}</h3><ul>`
@@ -208,30 +208,6 @@ function sendSignal(endpoint) {
         .catch((error) => console.log(error))
 }
 
-function writePopup(response, currentWork) {
-    let popUpWindow = document.getElementById('popup')
-    popUpWindow.innerHTML = ``
-    let listOfNames = response.data
-    for (let i = 0;i<listOfNames.length;i++) {
-        currentWork += `<option value='${listOfNames[i].id}'>${listOfNames[i].proper_name}</option>`
-    }
-    currentWork += `</div>`
-    currentWork += `</select><button class='popup' id='submit-to-table'>Submit</button>`
-    popUpWindow.innerHTML = currentWork
-    //console.log(currentWork) //TODO add button to popup and be sure to pass along the id of the current selected so we can get the right thing added to the right table
-    let submitButton = document.getElementById('submit-to-table')
-    //console.log(submitButton)
-    submitButton.onclick = ev => {
-        let selectionBox = document.getElementById('selection')
-        postToConnectiveTable(selectionBox.value)
-    }
-    let popupBackground = document.getElementById('popupbkg')
-    popupBackground.onclick = ev => {
-        // console.log('click')
-        closeAddingWindow()
-    }
-}
-
 function formatNormal(string) {
     let stringArr = string.split('_')
     let formattedArr = []
@@ -248,20 +224,7 @@ function closeAddingWindow() {
     popUpWindow.innerHTML = ``
 }
 
-function postToConnectiveTable(selectedID) { // TODO Add function to server.py that returns a list of traits that are tracked in pgsql so we can gather them.
-    const loreconn = "";
-    let postData = {
-        'currentOpen': currentOpen,
-        'selectedId': selectedID
-    }
-    axios.post(loreconn + '/test-data-post', postData)
-        .then(response => {
-            // console.log(response)
-        })
-        .catch((error) => console.log(error))
-}
-
-function dropdownBuilder(htmlString, dataList, dataName) {
+function dropdownBuilder(htmlString, dataList, dataName, itemNum) {
     htmlString += `<label for="${dataName}">${formatNormal(dataName)}:</label>`
     htmlString += `<select name="${dataName}" id="selection-${dataName}">`
     // console.log(dataList)
@@ -326,7 +289,7 @@ function inputBoxBuilderNum(dataNameStr, initialValue) {
 async function queryConnectiveAndEndcap(value) {}
 
 
-async function extraDataQuerier(currentRequested, itemNum) {
+async function foreignDataQuerier(currentRequested, itemNum) {
     let htmlWork = ``
     for (const [key, value] of Object.entries(currentRequested['foreign_keyed'])) {
         if (value[1] == 'id') {
@@ -338,7 +301,7 @@ async function extraDataQuerier(currentRequested, itemNum) {
             htmlWork += dropdownBuilder('', returnedData.data, value[1])
         } else if (value[1] != currentOpen['table'].slice(1) + '_id') {
             let returnedData = await getEndcapData(value[1]).then(data => {return data})
-            htmlWork += dropdownBuilder('', returnedData, value[1])
+            htmlWork += dropdownBuilder('', returnedData, value[1], itemNum)
         }
     }
     return htmlWork
@@ -391,7 +354,7 @@ async function popupBuilderArbiter(tableSelected, itemNum) {
         console.log(error)
     }
     try {
-        htmlString += await extraDataQuerier(currentRequested, itemNum)
+        htmlString += await foreignDataQuerier(currentRequested, itemNum)
     } catch (error) {
         console.log(error)
     }
@@ -399,10 +362,13 @@ async function popupBuilderArbiter(tableSelected, itemNum) {
 
     htmlString += `<input type='submit' id='popup-submit' value='Submit'></form></div></div></div>`
     popUpWindow.innerHTML = htmlString
-    // let submitButtonPopup = document.getElementById('popup-submit')
-    // submitButtonPopup.onclick = ev => {
-    //     getForm()
-    // }
+    for (const [key, value] of Object.entries(currentRequested['foreign_keyed'])) {
+        if(value[1] == 'id') {
+            continue
+        }
+        let tempSelection = document.getElementById('selection-'+value[1])
+        tempSelection.selectedIndex = parseInt(openTableRows[itemNum][key]) - 1
+    }
     let popupBackground = document.getElementById('popupbkg')
     popupBackground.onclick = ev => {
         closeAddingWindow()
